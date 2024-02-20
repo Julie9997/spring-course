6ef7821fc9c96e633d872e28ec4915c4ece61485
Julie9997 <ula.9997@mail.ru>
2024-02-21 01:08:17 +0300

    lesson 9

---
+++ lesson9/example_gateway/microservice-1/src/test/java/ru/julie/microservice1/Microservice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.julie.microservice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microservice1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ lesson9/example_gateway/src/test/java/ru/julie/example_gateway/ExampleGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.julie.example_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
